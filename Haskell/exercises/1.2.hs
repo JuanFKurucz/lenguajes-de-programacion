b52bb622466030a8126596007142511cb67c9b2c
Juan Francisco Kurucz <juanfkurucz@gmail.com>
2020-08-21 18:31:14 -0300

    implementation of excerise 1.2

---
+++ Haskell/exercises/1.2.hs
@@ -0,0 +1,28 @@
+-- Define a method called stringify
+-- It should recieve a JSON type and return a String with the formatted JSON
+
+import Data.List
+import Data.Map
+import Data.Tuple
+import Data.Map.Strict (Map, fromList)
+import Data.Char
+
+data JSONValue
+  = JSONNull | JSONBool Bool | JSONNumber Double
+  | JSONString String | JSONArray [JSONValue]
+  | JSONObject (Map String JSONValue)
+  deriving (Eq, Show)
+
+
+stringify :: JSONValue -> String
+stringify (JSONArray []) = "[]"
+stringify JSONNull = "null"
+stringify (JSONBool x) = show x 
+stringify (JSONString x) = show x
+stringify (JSONNumber x) = show x
+stringify (JSONArray x) = "[" ++ intercalate "," (Prelude.map stringify x) ++ "]"
+stringify (JSONObject m) = "{" ++ intercalate "," [ (\ x y -> show x  ++ ":" ++ stringify y) k v | (k, v) <- tuplas] ++ "}"
+      where 
+        tuplas = Data.Map.toList m
+
+main = putStrLn (stringify (JSONObject (Data.Map.fromList [("x", JSONNumber 1.0),("y", JSONString "2"), ("list", JSONArray [JSONNull])])))
