eb5126a0391dcfb7193b8552ce2376d5e8829860
Diego Bergara <37369075+DiegoBergara@users.noreply.github.com>
2020-08-27 20:53:24 -0300

    Tautology : First attempt

--- Haskell/exercises/2.1.hs
+++ Haskell/exercises/2.1.hs
@@ -44,47 +44,53 @@ propVars (OpBI p1 p2) = concat(propVars p1, propVars p2)
 assigns :: Prop -> [VarAsign]
 assigns p =  (nub [fromList [(x,y)] | x <- (propVars p), y <- [True, False]])
 
---Genera asignaciones aleatorias de valores de verdad a variables
-randomVarAsign :: [String] -> IO VarAsign
-randomVarAsign [] = return Map.empty
-randomVarAsign (x:xs) = do var <- randomRIO (True, False)
-                           cs <- randomVarAsign (xs)
-                           return (Map.union (Map.insert x var Map.empty) cs)
+-- --Genera asignaciones aleatorias de valores de verdad a variables
+-- randomVarAsign :: [String] -> IO VarAsign
+-- randomVarAsign [] = return Map.empty
+-- randomVarAsign (x:xs) = do var <- randomRIO (True, False)
+--                            cs <- randomVarAsign (xs)
+--                            return (Map.union (Map.insert x var Map.empty) cs)
 
--- Definir un generador de proposiciones (tipo Prop) aleatorias: 
--- Limitar la altura del árbol. 
-randomProp :: Int -> IO Prop 
-randomProp 0 = do
-               rb <- randomRIO (True,False)
-               return (TruthValue rb)
-randomProp x = do
-                r <- randomRIO(0, 5)
-                case r ::  Int of
-                  0 -> do
-                        rp <- randomProp (x-1)
-                        return (OpNOT rp)
-                  1 -> do
-                        rp1 <- randomProp (x-1)
-                        rp2 <- randomProp (x-1)
-                        return (OpAND rp1 rp2)
-                  2 -> do
-                        rp1 <- randomProp (x-1)
-                        rp2 <- randomProp (x-1)
-                        return (OpOR rp1 rp2)
-                  3 -> do
-                        rp1 <- randomProp (x-1)
-                        rp2 <- randomProp (x-1)
-                        return (OpIF rp1 rp2)
-                  4 -> do
-                        rp1 <- randomProp (x-1)
-                        rp2 <- randomProp (x-1)
-                        return (OpIFF rp1 rp2)
-                  5 -> do
-                        rb <- randomRIO (True,False)
-                        return (TruthValue rb)
+-- -- Definir un generador de proposiciones (tipo Prop) aleatorias: 
+-- -- Limitar la altura del árbol. 
+-- randomProp :: Int -> IO Prop 
+-- randomProp 0 = do
+--                rb <- randomRIO (True,False)
+--                return (TruthValue rb)
+-- randomProp x = do
+--                 r <- randomRIO(0, 5)
+--                 case r ::  Int of
+--                   0 -> do
+--                         rp <- randomProp (x-1)
+--                         return (OpNOT rp)
+--                   1 -> do
+--                         rp1 <- randomProp (x-1)
+--                         rp2 <- randomProp (x-1)
+--                         return (OpAND rp1 rp2)
+--                   2 -> do
+--                         rp1 <- randomProp (x-1)
+--                         rp2 <- randomProp (x-1)
+--                         return (OpOR rp1 rp2)
+--                   3 -> do
+--                         rp1 <- randomProp (x-1)
+--                         rp2 <- randomProp (x-1)
+--                         return (OpIF rp1 rp2)
+--                   4 -> do
+--                         rp1 <- randomProp (x-1)
+--                         rp2 <- randomProp (x-1)
+--                         return (OpIFF rp1 rp2)
+--                   5 -> do
+--                         rb <- randomRIO (True,False)
+--                         return (TruthValue rb)
 
+tautology :: Prop -> Bool
+tautology (TruthValue a) = a
+tautology prop =  if check then and (map (eval prop) varAssigns) else eval prop (fromList [])
+    where 
+      varAssigns = assigns prop
+      check = length varAssigns > 0
 
 -- Calcula si una proposición es una tautologia (Todas las combinaciones de variables dan como resultado True)
 --tautology :: Prop -> Bool
 
-main = putStrLn "hola"
+main = print (tautology (OpNOT (TruthValue True)))
