08f2f5db5d94fd621d14d10b2d30a7741f588a85
Javier Martin <inakiuy@users.noreply.github.com>
2020-09-25 17:47:25 -0300

    funcion de referencia toma string y lo separa por un delimitador. Devuelve array de strings

---
+++ C/split.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <string.h>
+
+#define LARGO 100
+char** split(char * originStringPointer, char * delimiterStringPointer, char **resultArrayPointer, int size);
+
+/*
+ * Main ej 1.4
+ * 
+ */
+int main( ) {
+    // declaraciones
+    char *originString;
+    char *resultArray[LARGO];
+    char cadenaNaturales[LARGO];
+    int i;
+
+
+    printf( "Ingresa numeros naturales separados por espacio : ");
+    fgets(cadenaNaturales, LARGO, stdin);
+    // Duplico el string
+    originString = strdup(cadenaNaturales);
+
+    // Separo el string en tokens
+    split(originString, " ", resultArray, LARGO);
+
+    // Imprimo el array de strings para ver el resultado
+    for (i = 0; i < LARGO; i++){
+        printf("String = %s", resultArray[i] );
+        printf("\tAddress of string literal = %p\n", resultArray[i]);
+    }
+
+    return 0;
+}
+
+/*
+ * Funcion que toma un string (puntero) y un delimitador y separa
+ * el string.
+ * Retorna un array con los tokens.
+ */
+char** split(char * originStringPointer, char * delimiterStringPointer, char **resultArrayPointer, int size){
+    
+    // Declaraciones
+    char *tokenPointer;
+    char *token[LARGO];
+    int  i;
+
+    i = 0;
+    // Extraer tokens y ponerlos en un array de strings uno a uno. Mientras hay espacio en el array de salida y
+    // el puntero al token es distinto de nulo
+    while(( i < size ) && ((tokenPointer = strsep(&originStringPointer, delimiterStringPointer)) != NULL) ){
+        resultArrayPointer[i] = strdup(tokenPointer);
+        printf("SPLIT: %s\n", tokenPointer);
+        
+        i++;
+    }
+
+    //printf("SPLIT RESULT: %s\n", resultArrayPointer);
+    return resultArrayPointer;
+}
