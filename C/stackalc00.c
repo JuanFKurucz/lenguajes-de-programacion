9cb5ba94f064202c763e48b25c8d0baf00987175
Juan Francisco Kurucz <juanfkurucz@gmail.com>
2020-10-01 09:47:28 -0300

    fix missing strsep declaration and strcmp comparison

--- C/stackalc00.c
+++ C/stackalc00.c
@@ -5,22 +5,38 @@
 
 // Declarar funciones
 int evaluate(char **code, int *pos, int *stack, int *top);
-char** split(char * originStringPointer, char * delimiterStringPointer, char **resultArrayPointer, int size);
+char **split(char *originStringPointer, char *delimiterStringPointer, char **resultArrayPointer, int size);
 int add(int a, int b);
 int sub(int a, int b);
 int mult(int a, int b);
 int div(int a, int b);
 int pusheye(int elem, int *stack, int *top);
 int popeye(int *stack, int *top);
+char *strsep(char **stringp, const char *delim);
+
+char *strsep(char **stringp, const char *delim)
+{
+    char *rv = *stringp;
+    if (rv)
+    {
+        *stringp += strcspn(*stringp, delim);
+        if (**stringp)
+            *(*stringp)++ = '\0';
+        else
+            *stringp = 0;
+    }
+    return rv;
+}
 
 /*
  * Main ej 2.2
  * 
  */
-int main( ) {
+int main()
+{
     // Declarar var
     char *code[LARGO] = {"ADD"};
-    int pos, stack[LARGO], top;  
+    int pos, stack[LARGO], top;
 
     // inicalizamos
     pos = 0;
@@ -36,36 +52,37 @@ int main( ) {
  * 
  * 
  */
-int evaluate(char **code, int *pos, int *stack, int *top){
+int evaluate(char **code, int *pos, int *stack, int *top)
+{
     // Declarar variables
     char *instruction;
     char *instruction_parsed[2];
     int a, b, z;
     // Agarra la intrucción en la pos
     instruction = code[*pos];
- 
-    if (strcmp("ADD",instruction)){
+    if (strcmp("ADD", instruction) == 0)
+    {
         printf("Entramos en ADD\n");
         a = popeye(stack, top);
         b = popeye(stack, top);
         // chequear que a y b tengan valores
-        return pusheye( a + b, stack, top);
+        return pusheye(a + b, stack, top);
     }
-    else if (strcmp("DIV",instruction))
+    else if (strcmp("DIV", instruction) == 0)
     {
-      printf("Entramos en DIV\n");
-      a = popeye(stack, top);
-      b = popeye(stack, top);
-      return pusheye(a / b , stack, top);
+        printf("Entramos en DIV\n");
+        a = popeye(stack, top);
+        b = popeye(stack, top);
+        return pusheye(a / b, stack, top);
     }
-    else if (strcmp("SUB",instruction))
+    else if (strcmp("SUB", instruction) == 0)
     {
         printf("Entramos en SUB\n");
         a = popeye(stack, top);
         b = popeye(stack, top);
         return pusheye(a - b, stack, top);
     }
-    else if (strcmp("MULT",instruction))
+    else if (strcmp("MULT", instruction) == 0)
     {
         printf("Entramos en MULT\n");
         a = popeye(stack, top);
@@ -74,8 +91,8 @@ int evaluate(char **code, int *pos, int *stack, int *top){
     }
     else
     {
-        split(instruction,"-", instruction_parsed,2);
-        sscanf(instruction_parsed[1],"%d",&z);
+        split(instruction, "-", instruction_parsed, 2);
+        sscanf(instruction_parsed[1], "%d", &z);
         pusheye(z, stack, top);
     }
     pos++;
@@ -86,20 +103,22 @@ int evaluate(char **code, int *pos, int *stack, int *top){
  * el string.
  * Retorna un array con los tokens.
  */
-char** split(char * originStringPointer, char * delimiterStringPointer, char **resultArrayPointer, int size){
-    
+char **split(char *originStringPointer, char *delimiterStringPointer, char **resultArrayPointer, int size)
+{
+
     // Declaraciones
     char *tokenPointer;
     char *token[LARGO];
-    int  i;
+    int i;
 
     i = 0;
     // Extraer tokens y ponerlos en un array de strings uno a uno. Mientras hay espacio en el array de salida y
     // el puntero al token es distinto de nulo
-    while(( i < size ) && ((tokenPointer = strsep(&originStringPointer, delimiterStringPointer)) != NULL) ){
+    while ((i < size) && ((tokenPointer = strsep(&originStringPointer, delimiterStringPointer)) != NULL))
+    {
         resultArrayPointer[i] = strdup(tokenPointer);
         printf("SPLIT: %s\n", tokenPointer);
-        
+
         i++;
     }
 
@@ -107,12 +126,14 @@ char** split(char * originStringPointer, char * delimiterStringPointer, char **r
     return resultArrayPointer;
 }
 
-int pusheye(int elem, int *stack, int *top){
+int pusheye(int elem, int *stack, int *top)
+{
     *top++;
     stack[*top] = elem;
     return 0;
 }
 
-int popeye(int *stack, int *top){
-    return stack[*top--];    
+int popeye(int *stack, int *top)
+{
+    return stack[*top--];
 }
